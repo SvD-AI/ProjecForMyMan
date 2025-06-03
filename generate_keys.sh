7d61c101067d1289ce627816f2eb1326fbc60c67
Святослав Дробишевський <clavik55582@stud.op.edu.ua>
2025-06-04 02:01:50 +0300

    Delete generate_keys.sh

--- generate_keys.sh
+++
@@ -1,9 +0,0 @@
-openssl genrsa -out keypair.pem 2048
-
-mkdir "src/main/resources/certs"
-
-openssl rsa -in keypair.pem -pubout -out "src/main/resources/certs/public.pem"
-
-openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in keypair.pem -out "src/main/resources/certs/private.pem"
-
-rm keypair.pem
